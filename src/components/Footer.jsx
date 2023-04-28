062332422eb3fe274b6c9ca2da5df88f9a09e81a
nnatroo <nugonatroshvili@gmail.com>
2023-04-28 20:05:47 +0400

    modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,39 @@
-import classes from '../modules/Footer.module.css'
+import classes from "../modules/Footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={classes['footer']}>
-        <div className={classes["col-1"]}>
-
+    <div className={classes["footer-section"]}>
+      <div className={classes["footer"]}>
+        <a className={classes["col-1"]}>
+          <img src="../src\assets\logoipsum-283.svg" alt="" />
+          <h3>Acme Store</h3>
+        </a>
+        <div className={classes["col-2"]}>
+          <a>Home</a>
+          <a>About</a>
+          <a>Terms & Conditions</a>
+          <a>Shipping & Return Policy</a>
         </div>
+        <div className={classes["col-3"]}>
+          <a>Privacy Policy</a>
+          <a>FAQ</a>
+        </div>
+        <div className={classes["col-4"]}>
+          <a href="">
+            <img
+              src="../src\assets\github-142-svgrepo-com.svg"
+              width={"30px"}
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+      <hr />
+      <div className={classes['underfooter-section']}>
+        <span>© 2023 Natroshvili Store. All rights reserved.</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
